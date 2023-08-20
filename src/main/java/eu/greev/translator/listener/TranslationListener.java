6a6fb72eb2eed0f9db0ef91b1b87a11ae839c464
JJJannik <jannikjentsch2006@gmail.com>
2023-08-20 15:13:32 +0200

    Add cooldown logic

--- src/main/java/eu/greev/translator/listener/TranslationListener.java
+++ src/main/java/eu/greev/translator/listener/TranslationListener.java
@@ -4,7 +4,6 @@ import com.deepl.api.DeepLException;
 import com.deepl.api.TextResult;
 import com.deepl.api.Translator;
 import com.deepl.api.Usage;
-import eu.greev.translator.Main;
 import eu.greev.translator.classes.TranslationLimitReachedException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -19,15 +18,20 @@ import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import net.dv8tion.jda.api.interactions.commands.Command;
 
 import java.awt.*;
+import java.time.Instant;
 import java.util.HashMap;
+import java.util.Timer;
+import java.util.TimerTask;
 import java.util.concurrent.TimeUnit;
 
 @Slf4j
 @RequiredArgsConstructor
 public class TranslationListener extends ListenerAdapter {
     private final MessageEmbed defaultEmbed = new EmbedBuilder().setColor(new Color(63, 226, 69, 255)).build();
-    private final HashMap<Message, Long> alreadyTranslated = new HashMap<>();
-    private final Translator translator = Main.getTranslator();
+    private final HashMap<Long, Instant> alreadyTranslated = new HashMap<>();
+    private final Translator translator;
+    private final int cooldownMinutes;
+    private final int maxMessages;
 
     @Override
     public void onMessageContextInteraction(MessageContextInteractionEvent event) {
@@ -36,7 +40,23 @@ public class TranslationListener extends ListenerAdapter {
                 || event.getTarget().getAuthor().equals(jda.getSelfUser())) {
             return;
         }
+
         Message message = event.getTarget();
+        if (alreadyTranslated.containsKey(message.getIdLong())) {
+            event.replyEmbeds(new EmbedBuilder(defaultEmbed)
+                    .setDescription("This message already got recently public translated.")
+                    .build()
+            ).setEphemeral(true).queue();
+            return;
+        }
+
+        if (alreadyTranslated.size() >= maxMessages) {
+            event.reply(String.format("The maximum of %s translated messages per %d minutes has been reached, please wait until you can use translations again", maxMessages, cooldownMinutes))
+                    .setEphemeral(true)
+                    .queue();
+            return;
+        }
+
         TextResult result;
         try {
             result = getTranslatedResult(message, event.getUser().getName());
@@ -54,7 +74,10 @@ public class TranslationListener extends ListenerAdapter {
             return;
         }
 
-        message.reply(result.getText()).queue();
+        message.reply(result.getText()).queue(s -> {
+            alreadyTranslated.put(s.getIdLong(), Instant.now());
+            startCooldownTimer(s.getIdLong());
+        });
         event.replyEmbeds(new EmbedBuilder(defaultEmbed)
                 .setDescription(String.format("Successfully translated text from `%s` to `EN-GB`", result.getDetectedSourceLanguage()))
                 .build()
@@ -63,24 +86,45 @@ public class TranslationListener extends ListenerAdapter {
 
     @Override
     public void onMessageReceived(MessageReceivedEvent event) {
+        Message message = event.getMessage();
         JDA jda = event.getJDA();
-        MessageReference reference = event.getMessage().getMessageReference();
-        if (!event.getMessage().getMentions().isMentioned(jda.getSelfUser(), Message.MentionType.USER)
-                || reference == null || event.getAuthor().equals(jda.getSelfUser())) return;
 
-        Message message = reference.resolve().complete();
+        MessageReference reference = message.getMessageReference();
+        if (!message.getMentions().isMentioned(jda.getSelfUser(), Message.MentionType.USER) || reference == null
+                || event.getAuthor().equals(jda.getSelfUser())) {
+            return;
+        }
+
+        Message target = reference.resolve().complete();
+        if (alreadyTranslated.containsKey(target.getIdLong())) {
+            message.replyEmbeds(new EmbedBuilder(defaultEmbed)
+                    .setDescription("This message already got recently public translated.")
+                    .build()
+            ).queue(s -> s.delete().queueAfter(1, TimeUnit.MINUTES));
+            return;
+        }
+
+        if (alreadyTranslated.size() >= maxMessages) {
+            message.reply(String.format("The maximum of %s translated messages per %d minutes has been reached, please wait until you can use translations again", maxMessages, cooldownMinutes))
+                    .queue(s -> s.delete().queueAfter(1, TimeUnit.MINUTES));
+            return;
+        }
+
         TextResult result;
         try {
-            result = getTranslatedResult(message, event.getAuthor().getName());
+            result = getTranslatedResult(target, event.getAuthor().getName());
         } catch (TranslationLimitReachedException e) {
-            event.getMessage().reply("Translating this message would exceed the monthly translation limit.").queue(s -> s.delete().queueAfter(1, TimeUnit.MINUTES));
+            message.reply("Translating this message would exceed the monthly translation limit.").queue(s -> s.delete().queueAfter(1, TimeUnit.MINUTES));
             return;
         }
         if (result == null) {
-            event.getMessage().reply("Could not translate text, please contact the server owner!").queue();
+            message.reply("Could not translate text, please contact the server owner!").queue();
             return;
         }
-        message.reply(result.getText()).queue();
+        target.reply(result.getText()).queue(s -> {
+            alreadyTranslated.put(s.getIdLong(), Instant.now());
+            startCooldownTimer(s.getIdLong());
+        });
     }
 
     private TextResult getTranslatedResult(Message message, String requester) throws TranslationLimitReachedException {
@@ -96,5 +140,14 @@ public class TranslationListener extends ListenerAdapter {
             log.error("Could not translate text:", e);
         }
         return null;
+    }
+
+    private void startCooldownTimer(long messageId) {
+        new Timer().schedule(new TimerTask() {
+            @Override
+            public void run() {
+                alreadyTranslated.remove(messageId);
+            }
+        }, 0, TimeUnit.MINUTES.toMillis(cooldownMinutes));
     }
 }
